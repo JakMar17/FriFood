b22cfb4e801ff43c3f69216fc398de03483518ae
KraljSersa <david.kralj.s@gmail.com>
2019-12-04 14:05:11 +0100

    ADD restaurant add fixes

--- FRIFOOD/app_server/routes/restaurant-add.js
+++ FRIFOOD/app_server/routes/restaurant-add.js
@@ -5,4 +5,21 @@ var ctrlMain = require('../controllers/main');
 // rout to restaurant list
 router.get('/', ctrlMain.restaurantAdd);
 
+router.post('/addRestaurant', function (req, res) {
+
+    if(req.body.newCommentText != undefined)
+    {
+        var commentsJSON = require('../models/coments');
+
+        commentsJSON.komentarji.push({
+            "value": req.body.newCommentText.toString(),
+            "naslov": "Empty",
+        });
+
+        res.redirect("/commentPage");
+    }
+    else
+        res.status(401).send("No text entered");
+});
+
 module.exports = router;
