7e4dc893fcfb6a414470c12632bbbfd78e58fb04
KraljSersa <david.kralj.s@gmail.com>
2019-12-04 23:51:42 +0100

    ADD location based search for restaurants, save them in a list

--- FRIFOOD/public/javascripts/restaurant-list-helper.js
+++ FRIFOOD/public/javascripts/restaurant-list-helper.js
@@ -47,7 +47,11 @@ var ljubljana = {
     lng: 14.505751
 };
 
+var returnRestaurants = [];
+
 function initMap() {
+
+    document.getElementById("showHideFoundRestaurants").style.display = "none";
 
     map = new google.maps.Map(document.getElementById('googleMap'), {
         center: {lat: -46.056946 , lng: 14.505751},
@@ -70,24 +74,74 @@ function initMap() {
             map.setCenter(pos);
         }, function() {
             // if user can not be located, set ljubljana as the map center
-            handleLocationError(true, infoWindow, map.setCenter(ljubljana));
+            map.zoom = 12;
+            map.setCenter(ljubljana);
         });
     } else {
         // if user can not be located, set ljubljana as the map center
-        handleLocationError(false, infoWindow, map.setCenter(ljubljana));
+        map.zoom = 12;
+        map.setCenter(ljubljana);
     }
+
+    google.maps.event.addListener(map, "click", function (event) {
+        var latitude = event.latLng.lat();
+        var longitude = event.latLng.lng();
+        console.log( latitude + ', ' + longitude );
+
+        radius = new google.maps.Circle({map: map,
+            radius: 300,
+            center: event.latLng,
+            fillColor: '#777',
+            fillOpacity: 0.1,
+            strokeColor: '#AA0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            draggable: false,    // Dragable
+            editable: false      // Resizable
+        });
+
+        // Center of map
+        var location = new google.maps.LatLng(latitude, longitude);
+        map.panTo(location);
+        map.setCenter(location);
+
+        var request = {
+            location: location,
+            radius: 300,
+            query: 'Restavracije',
+            fields: ['name', 'geometry', 'place_id', 'formatted_address', 'photos'],
+        };
+
+        var service = new google.maps.places.PlacesService(map);
+        service.textSearch(request, function(results, status) {
+            //console.log(status === google.maps.places.PlacesServiceStatus.ZERO_RESULTS);
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                var resultCount = 0;
+                for (var i = 0; i < results.length; i++) {
+                    if (google.maps.geometry.spherical.computeDistanceBetween(results[i].geometry.location, location) < request.radius) {
+                        returnRestaurants.push(results[i]);
+                        createMarker(results[i]);
+                        document.getElementById("showHideRestaurants").style.display = "none";
+                        document.getElementById("showHideFoundRestaurants").style.display = "";
+                        resultCount++;
+                    }
+
+                }
+                // for (var i = 0; i < results.length; i++) {
+                //     console.log(results[i].name);
+                //     createMarker(results[i]);
+                // }
+                map.setCenter(results[0].geometry.location);
+                console.log(returnRestaurants);
+            }
+            else {
+                window.alert("Sorry, this location was not found.\nYou will be redirected to your current location.");
+                initMap();
+            }
+        });
+
+    });
 }
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-}
-
-
-
 
 
 
