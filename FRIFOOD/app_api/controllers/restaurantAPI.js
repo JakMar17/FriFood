cc1ca8a28755a3acb6e1c40d9283016751107a23
JakMar17 <jakob.marusic17@gmail.com>
2019-12-07 12:48:02 +0100

    working on admin dashboard

--- FRIFOOD/app_api/controllers/restaurantAPI.js
+++ FRIFOOD/app_api/controllers/restaurantAPI.js
@@ -105,6 +105,9 @@ const readRestaurants = (req, res) => {
 };
 
 const deleteRestaurant = (req, res) => {
+
+    console.log(req.body);
+
     var id = req.body.restaurantID.toString();
     var ObjectID = mongoose.Types.ObjectId;
 
