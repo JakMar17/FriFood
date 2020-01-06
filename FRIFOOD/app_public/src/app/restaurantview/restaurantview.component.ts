b768855b209262f91e8365d84414c3514308973c
zigasebenik <ziga.sebenik39@gmail.com>
2020-01-06 21:40:02 +0100

    added commentpage

---
+++ FRIFOOD/app_public/src/app/restaurantview/restaurantview.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurantview',
+  templateUrl: './restaurantview.component.html',
+  styleUrls: ['./restaurantview.component.css']
+})
+export class RestaurantviewComponent implements OnInit {
+
+  daysOfWork = {
+    week: ["PON", "TOR", "SRE", "ČET", "PET", "SOB", "NED"]
+  }
+  restaurant = {
+    _id: "5debddcf5fc3683918ae71a6",
+    name: "Restavracija 123",
+    description: "dodaj opis restavracije",
+    timeTable: [ "7h - 15h", "7h - 15h", "7h - 15h", "7h - 15h", "7h - 15h" ],
+    idKomentarjev: "idZaKomentarje",
+
+    front: "naslovna123.png"
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+export class Restaurant {
+  _id: string;
+  name: string;
+  description: string;
+  timeTable: string[];
+  idKomentarjev: string;
+
+  front: string;
+}
