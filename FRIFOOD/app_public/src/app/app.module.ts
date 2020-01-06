999c31e4483064b8c07483a65c2c334af5e6a18d
zigasebenik <ziga.sebenik39@gmail.com>
2020-01-06 13:50:05 +0100

    added routing

--- FRIFOOD/app_public/src/app/app.module.ts
+++ FRIFOOD/app_public/src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { OgrodjeComponent } from './ogrodje/ogrodje.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OgrodjeComponent,
+    FrontpageComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'register',
+        component: RegisterComponent
+      },
+      {
+        path: '',
+        component: FrontpageComponent
+      }
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [OgrodjeComponent]
 })
 export class AppModule { }
