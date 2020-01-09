3d2af0b5e297b76319c5191276f5113315aea5fe
zigasebenik <ziga.sebenik39@gmail.com>
2020-01-09 02:35:31 +0100

    restructured

--- FRIFOOD/app_public/src/app/app.module.ts
+++ FRIFOOD/app_public/src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+
+import { AppUsmerjanjeModule } from './modules/app-usmerjanje/app-usmerjanje.module';
 
 import { AppComponent } from './app.component';
-import { OgrodjeComponent } from './ogrodje/ogrodje.component';
-import { FrontpageComponent } from './frontpage/frontpage.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { CommentpageComponent } from './commentpage/commentpage.component';
-import { RestaurantviewComponent } from './restaurantview/restaurantview.component';
-import { CommentComponent } from './commentpage/comment/comment.component';
-import {FormsModule} from "@angular/forms";
+import { OgrodjeComponent } from './components/ogrodje/ogrodje.component';
+import { FrontpageComponent } from './components/frontpage/frontpage.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { CommentpageComponent } from './components/commentpage/commentpage.component';
+import { RestaurantviewComponent } from './components/restaurantview/restaurantview.component';
+import { CommentComponent } from './components/commentpage/comment/comment.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {NgbModalBackdrop} from "@ng-bootstrap/ng-bootstrap/modal/modal-backdrop";
 import {NgbModalModule, NgbModule} from "@ng-bootstrap/ng-bootstrap";
-
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -29,32 +30,12 @@ import {NgbModalModule, NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppUsmerjanjeModule,
     NgbModule,
-    NgbModalModule,
-    RouterModule.forRoot([
-      {
-        path: 'login',
-        component: LoginComponent
-      },
-
-      {
-        path: 'register',
-        component: RegisterComponent
-      },
-      {
-        path: 'commentPage/:id',
-        component: CommentpageComponent
-      },
-      {
-        path: 'restaurantView',
-        component: RestaurantviewComponent
-      },
-      {
-        path: '',
-        component: FrontpageComponent
-      }
-    ]),
-    FormsModule
+    NgbModalModule
   ],
   providers: [],
   bootstrap: [OgrodjeComponent]
---
+++ FRIFOOD/app_public/src/app/modules/app-usmerjanje/app-usmerjanje.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { FrontpageComponent } from '../../components/frontpage/frontpage.component';
+import { RegisterComponent } from '../../components/register/register.component';
+import { LoginComponent } from '../../components/login/login.component';
+import { CommentpageComponent } from '../../components/commentpage/commentpage.component';
+import { RestaurantviewComponent } from '../../components/restaurantview/restaurantview.component';
+
+
+const poti: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'commentPage/:id',
+    component: CommentpageComponent
+  },
+  {
+    path: 'restaurantView',
+    component: RestaurantviewComponent
+  },
+  {
+    path: '',
+    component: FrontpageComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(poti)
+  ],
+  exports: [RouterModule]
+})
+export class AppUsmerjanjeModule { }
