97056b71d18a813ddc86b5a66cb18bf25acbd472
KraljSersa <david.kralj.s@gmail.com>
2020-01-11 22:39:07 +0100

    ADD analytics

--- FRIFOOD/app_public/src/app/app.component.ts
+++ FRIFOOD/app_public/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {Router, NavigationEnd} from '@angular/router';
+declare let ga: Function;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FriFood';
+
+
+  constructor(public router: Router) {
+
+    // subscribe to router events and send page views to Google Analytics
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ga('set', 'page', event.urlAfterRedirects);
+        ga('send', 'pageview');
+      }
+    });
+  }
 }
